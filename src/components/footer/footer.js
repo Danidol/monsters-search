c3cbc56536ba96e585dba9b826dfe5dfc51182ed
Daniel <danicek76@yahoo.com>
2021-02-18 17:32:37 +0100

    minor update

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,7 +3,6 @@ import "./footer.css"
 export const Footer = () => {
   return (
     <div className="footer">
-      <button>Social</button>
       <a target="blanc" href="https://www.themadesign.net">
         themadesign.net 2021
       </a>
