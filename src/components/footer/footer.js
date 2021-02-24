1b36cd280017fe283fdd491229f83db178c54dad
Daniel <60354154+Danidol@users.noreply.github.com>
2021-02-24 22:56:14 +0100

    Update footer.js

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,7 +3,7 @@ import "./footer.css"
 export const Footer = () => {
   return (
     <div className="footer">
-      <a target="blanc" href="https://www.themadesign.net">
+      <a target="blank" href="https://www.themadesign.net">
         themadesign.net 2021
       </a>
     </div>
